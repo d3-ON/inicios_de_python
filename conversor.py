682fd15534386ab971789638a8ce84210849da14
deON <lopezgimenezdiego@gmail.com>
2021-08-19 14:44:24 -0400

    se agrega menu de monedas

--- conversor.py
+++ conversor.py
@@ -1,3 +1,12 @@
+menu = """
+Bienvenido al conversor de monedas 💴➡💱➡💵
+
+1- Pesos colombianos
+2- Pesos argentinos
+3- Pesos mexicanos
+
+Elige una opción"""
+
 dolar = float(input("Cuántos Dólares tienes? "))
 valor_guarani = 0.00014
 guarani = str(round(dolar / valor_guarani, 2))
