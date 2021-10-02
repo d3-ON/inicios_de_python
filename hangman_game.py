b9a71f3c8558b57f816d3bb847373351a85bc0d8
deON <lopezgimenezdiego@gmail.com>
2021-10-02 18:39:40 -0400

    add character cleaner function

--- hangman_game.py
+++ hangman_game.py
@@ -13,24 +13,49 @@ def words_list():
     return words_list
 
 
-def random_word(word):
-    aleatory_word = random.choice(word)
+def hidde_word(word):
+    hidde = ''
+    
+    for i in range(len(word)):
+        hidde += '_'
+    
+    return list(hidde)
+              
+
+def dict_word(word):
     dict_word = {}
     counter = 0
     
-    for i in aleatory_word:
+    for i in word:
         dict_word[counter] = i
         counter += 1   
         
     return dict_word
 
-          
+def letter_in():
+    letter = input('')
+
+
+def character_cleaner(word):
+    word = (word.replace("'","").replace(',','')
+            .replace('[','').replace(']',''))
+    
+    # clean = ''
+    # for i in word:
+    #     if i != '_' and i != ' ' and i != "letra que ingresa el usuario":
+    #         i = i.replace(i, '')
+    #     clean += i
+
+    return word
+
 
 def run():
-    step_1 = words_list()
-    step_2 = random_word(step_1)
+    step_1 = random.choice(words_list())
+    step_2 = str(hidde_word(step_1))
+    step_3 = dict_word(step_1)
     
-
+    print(character_cleaner(step_2))
+    print(step_3)
 
 if __name__ == '__main__':
     run()
